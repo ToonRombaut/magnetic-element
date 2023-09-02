67d3f4efee2eeeda591985df993f1ee1ce5e69a7
Toon Rombaut <toon.rombaut@hotmail.com>
2023-09-02 10:48:23 +0200

    fixed bug that resetted element to quick;

--- src/MagneticElementsController/magneticElementsController.js
+++ src/MagneticElementsController/magneticElementsController.js
@@ -102,8 +102,8 @@ export default class MagneticElementsController{
             element.x.target = targetPosition.x * this.magneticForce;
             element.y.target = targetPosition.y * this.magneticForce;
 
-            if(element.x.current < .1 && element.x.target == 0) element.x.current = 0;
-            if(element.y.current < .1 && element.y.target == 0) element.y.current = 0;
+            if(Math.abs(element.x.current) < .01 && element.x.target == 0) element.x.current = 0;
+            if(Math.abs(element.y.current) < .01 && element.y.target == 0) element.y.current = 0;
             
             element.x.current = this.lerp(element.x.current,element.x.target,this.interpolationFactor,delta);
             element.y.current = this.lerp(element.y.current,element.y.target,this.interpolationFactor,delta);
